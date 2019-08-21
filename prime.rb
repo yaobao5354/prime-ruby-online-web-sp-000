a33eb4decf730eb88734bde90acb900f31d34ef0
yaoyu bao <yaobao5354@gmail.com>
2019-08-21 21:23:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'pry'
+def prime?(integer)
+  if integer <= 1  
+    return false 
+  elsif integer == 2 
+    return true
+  else 
+    array = (2..integer-1).to_a
+    factor_array = []
+    array.each do |number| 
+      if integer % number == 0
+        factor_array << number
+      end 
+    end
+    if factor_array.length > 0 
+      return false 
+    else 
+      return true 
+    end 
+  end 
+end 
