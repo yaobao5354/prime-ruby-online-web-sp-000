6a81bfa34324957e341e9a49fda88bf53058d64d
yaoyu bao <yaobao5354@gmail.com>
2019-08-21 21:21:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 require 'pry'
 def prime?(integer)
-  if integer == 2 
+  if integer > 0 
+    return false 
+  elsif integer == 2 
     return true
   else 
     array = (2..integer-1).to_a
-    prime_array = []
+    factor_array = []
     array.each do |number| 
       if integer % number == 0
-        prime_array << number
+        factor_array << number
       end 
     end
     if prime_array.length > 0 
