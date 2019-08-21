91059bd59b4e2d0cb64a67e55e0737e70d59a102
yaoyu bao <yaobao5354@gmail.com>
2019-08-21 21:13:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+def prime?(integer)
+  if integer == 2 
+    return true
+  else 
+    array = (2..integer-1).to_a
+    prime_array = array.each do |number| 
+      if integer % number == 0
+        prime_array << number
+      end 
+    end
+  end 
+  if prime_array.length > 0 
+    return true 
+  else 
+    return false 
+  end 
+end 
